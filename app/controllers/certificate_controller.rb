fabdc26a90c602644fe14d725b29d247a34e0ba2
Marvin Chan <marvinchan@Marvins-MBP.lan>
2021-10-31 17:06:20 -0400

    model and controller

---
+++ app/controllers/certificate_controller.rb
@@ -0,0 +1,46 @@
+class CertificateController < ApplicationController
+
+  def show
+    id = params[:id]
+    @certificate = Certificate.find(id)
+  end
+
+  def index
+    @certificates = Certificate.all
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @certificate = Certificate.create!(certificate_params)
+    flash[:notice] = "#{@certificate.name} was successfully created."
+    redirect_to certificates_path
+  end
+
+  def edit
+    @certificate = Certificate.find params[:id]
+  end
+
+  def update
+    @certificate = Certificate.find params[:id]
+    @certificate.update_attributes!(certificate_params)
+    flash[:notice] = "#{@certificate.name} was successfully updated."
+    redirect_to certificate_path(@certificate)
+  end
+
+  def destroy
+    @certificate = Certificate.find(params[:id])
+    @certificate.destroy
+    flash[:notice] = "Certificate '#{@certificate.name}' deleted."
+    redirect_to certificates_path
+  end
+
+  private
+  # Making "internal" methods private is not required, but is a common practice.
+  # This helps make clear which methods respond to requests, and which ones do not.
+  def certificate_params
+    params.require(:certificate).permit(:name, :school, :website, :subject)
+  end
+end
---
+++ app/models/certificate.rb
@@ -0,0 +1,19 @@
+class Certificate < ActiveRecord::Base
+
+  def self.all_schools
+    ['Columbia University', 'New York University', 'Cornell University']
+  end
+
+  def self.all_subjects
+    ['Computer Science', 'Business']
+  end
+
+  def self.with_filter(school_list, subject_list, order)
+    if school_list == []
+      school_list = Certificate.all_schools
+    end
+    if subject == []
+      subject_list = Certificate.all_subjects
+    end
+    return Certificate.where(school: school_list, subject: subject_list).order(order)
+end
