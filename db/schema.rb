a3766a1a4190dce47bf93ebc95a818efab1305af
Marvin Chan <marvinchan@Marvins-MBP.lan>
2021-11-01 15:26:59 -0400

    remove movies from schema

--- db/schema.rb
+++ db/schema.rb
@@ -20,13 +20,4 @@ ActiveRecord::Schema.define(version: 20211101065825) do
     t.string "website"
   end
 
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.string   "rating"
-    t.text     "description"
-    t.datetime "release_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
