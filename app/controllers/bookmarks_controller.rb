3ff9956d0087360ef5ef7c248bb8985b34408739
Marvin Chan <marvinchan@Marvins-MBP.lan>
2021-11-16 20:36:48 -0500

    bookmark redirect to certificate

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,41 +1,22 @@
 class BookmarksController < ApplicationController
+  before_action :require_login, only: [:create]
 
-  def new
-    cert_id = params[:certificate_id]
-    uname = session[:username]
-    @certificate = Certificate.find cert_id
-    @user = User.where(username: uname)
+  def create
+    @user = User.where(username: session[:username])
     if @user.empty?
       redirect_to new_session_path
       return
     end
     @user = @user.first
-    @bookmark = Bookmark.where(user_id: @user.id, certificate_id: @certificate)
+    cert_id = params[:bookmarks][:certificate_id]
+    @certificate = Certificate.find cert_id
+    @bookmark = Bookmark.where(user_id: @user.id, certificate_id: @certificate.id)
     if @bookmark.empty?
       flash[:notice] = "Added bookmark for course #{@certificate.name}!"
       @bookmark = Bookmark.add_bookmark(@user, @certificate)
     end
     
     redirect_to @certificate
-
-    # cert_id = params[:certificate_id]
-    # print(params)
-    # @certificate = Certificate.find cert_id
-    # uname = session[:username]
-    # @user = User.get_user(uname)
-    # if @user == false
-    #   flash[:notice] = "Please login before you attempt to add bookmarks."
-    #   redirect_to certificates_path
-    #   return
-    # else
-    #   @bookmark = Bookmark.create!(:user => @user, :certificate => @certificate)
-    #   flash[:notice] = "Added bookmark for course #{@certificate.name} !"
-    #   redirect_to certificates_path
-    # end
-    # #add bookmark here
-    # #this call does not work for some reason
-    # # @bookmark = Bookmark.add_bookmark(@certificate,@user)
-    # redirect_to certificates_path
   end
 
 end
