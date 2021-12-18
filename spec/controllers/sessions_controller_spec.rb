00673670f81f3231d91157ef1e4996f702796a77
alex-preau <preau.alexander@gmail.com>
2021-12-18 11:20:26 -0600

    all rspec pass

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,21 +2,11 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
-  describe 'GET new' do
-    it 'redirects to user view' do
-      user = User.create(:name => "Alexander", :username => "sessions_user",
-                   :password => "password1")
 
-      get :new, nil,{username: user.username}
-      expect(response).to redirect_to(user)
-      User.find_by(:username => user.username).destroy
-    end
-  end
 
   describe "GET create" do
 
     it 'redirects to new session path if invalid username or password' do
-
       get :create, sessions: {username: nil,password:nil}
       expect(flash[:error]).to match(/Username or password error/)
       expect(response).to redirect_to(login_url)
@@ -30,5 +20,26 @@ RSpec.describe SessionsController, type: :controller do
       User.find_by(:username => user.username).destroy
     end
   end
+
+
+
+  describe 'GET login' do
+    it 'redirects to user view' do
+      user = User.create(:name => "Alexander", :username => "sessions_user_2",
+                   :password => "password1")
+      get :login, nil, {username: user.username}
+      expect(response).to redirect_to(user)
+      User.find_by(:username => user.username).destroy
+    end
+  end
+
+  describe 'GET login' do
+    it 'redirects to user view' do
+      get :logout, nil, nil
+      expect(response).to redirect_to(login_url)
+    end
+  end
+
+
 
 end
--- spec/models/certificate_spec.rb
+++ spec/models/certificate_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe Certificate, type: :model do
   before(:all) do
     if Certificate.where(:school => "Cornell University").empty?
 
-        Certificate.create(:school => "Cornell University", :name => "SQL",
+        Certificate.create(:school => "Columbia University", :name => "SQL",
                      :subject => "Computer Science", :website => "http://test.com")
-        Certificate.create(:school => "Cornell University", :name => "C++",
+        Certificate.create(:school => "New York University", :name => "C++",
+                    :subject => "Computer Science", :website => "http://test.com")
+        Certificate.create(:school => "Cornell University", :name => "C",
                     :subject => "Computer Science", :website => "http://test.com")
 
     end
@@ -14,7 +16,7 @@ end
 
   describe "filter" do
     it "returns all certs if no filter added" do
-      certs = Certificate.with_filter([],[],0)
+      certs = Certificate.with_filter([],[])
       all_certs = Certificate.where(school: Certificate.all_schools, subject: Certificate.all_subjects)
       expect(certs.length()).to eq all_certs.length
     end
@@ -22,7 +24,7 @@ end
 
   describe "filter" do
     it "returns correctly filtered certs" do
-      certs = Certificate.with_filter(["Cornell University"],[],0)
+      certs = Certificate.with_filter(["Cornell University"],[])
       correct_certs = all_certs = Certificate.where(school: ['Cornell University'], subject: Certificate.all_subjects)
       expect(certs.length()).to eq correct_certs.length()
     #  Certificate.find_by(:name => "SQL").destroy
